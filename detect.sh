2bec5411d10e05da1359bab8563011c18c714f36
Brian Doan <brianddoan@gmail.com>
2021-06-20 13:31:44 +0700

    More implicit detect command

--- detect.sh
+++ detect.sh
@@ -1,2 +1,2 @@
 cd core
-./darknet detect yolov3.cfg yolov3.weights -out ../public/response/predictions
+./darknet detector test cfg/coco.data yolov3.cfg yolov3.weights -out ../public/response/predictions
