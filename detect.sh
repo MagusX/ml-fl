f81ccb7ee9c14460076af9a3542b201b8e958186
Brian Doan <brianddoan@gmail.com>
2021-06-25 16:24:09 +0700

    Add detect command for new model

--- detect.sh
+++ detect.sh
@@ -1,2 +1,3 @@
 cd core
-./darknet detector test cfg/coco.data yolov3.cfg yolov3.weights -out ../public/response/predictions
+# ./darknet detector test cfg/coco.data yolov3.cfg yolov3.weights -out ../public/response/predictions
+./darknet detector test cfg/obj.data yolo-custom.cfg yolo-custom3400.weights -out ../public/response/predictions
